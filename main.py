d73531fe6425153e47fdc5c5359b3707219ce787
Doug <dougl.wil@gmail.com>
2022-02-04 15:11:49 -0300

    Adding main.py file

---
+++ main.py
@@ -0,0 +1,8 @@
+título = input('Qual o seu título de livro favorito?: ')
+autor = input('Qual o nome do/a autor/a?: ')
+sobrenome = input('Qual o sobrenome?: ')
+ed = input('Qual a edição desse livro: ')
+ano = input('Qual o ano de publicação?: ')
+print('A sua referência do livro é: ', sobrenome.upper() + ', ' +
+      autor.capitalize() + '. ' + título.capitalize() + '. ' + ed + '. ed. ' + ano + '.')
+print('Faz ' + str(2022 - int(ano)) + ' ano(s) que esse livro foi lançado')
