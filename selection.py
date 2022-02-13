a938fc566f9ff85e5d47883008fd6ad81d16834b
Doug <dougl.wil@gmail.com>
2022-02-13 00:07:14 -0300

    Adicionando testes para estruturas de seleção

---
+++ selection.py
@@ -0,0 +1,16 @@
+opcao = int(input('Digite um número: '))
+
+menor_zero = opcao < 0
+maior_zero = opcao > 0
+igual_zero = opcao == 0
+
+while maior_zero:
+
+    if menor_zero:
+        print("A OPÇÃO SELECIONADA É MENOR QUE ZERO")
+    elif maior_zero:
+        print("A OPÇÃO SELECIONADA É MAIOR QUE ZERO")
+    else:
+        print("A OPÇÃO SELECIONADA É IGUAL A ZERO")
+
+    opcao = int(input('Digite um número: '))
